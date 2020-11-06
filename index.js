92d8410c046f5352d73c955c8dc3fbc178c7659f
Ester Uscanga <eusacanga@krasamo.com>
2020-11-06 17:00:14 -0600

    last commit

--- index.js
+++ index.js
@@ -18,18 +18,18 @@ app.use(bodyParser.json())
 
 app.get('/usuario/:matricula', async (req, res) => {
     const matricula = req.params.matricula
-    if(matricula){
+    if (matricula) {
         const obtenerUsuarioArea = `select usuarios.matricula, usuarios.sexo, usuarios.nombre, usuarios.apellido_paterno, usuarios.apellido_materno, usuarios.imss, areas.nombre as area 
                                     from usuarios, areas 
                                     where matricula = ${conn.escape(matricula)} and usuarios.area_id = areas.id;`
         conn.query(obtenerUsuarioArea, (error, result, fileds) => {
-            if(error) 
+            if (error)
                 res.status(500).send(error)
-            else 
+            else
                 res.status(200).send(result[0])
         })
     } else {
-        res.status(401).send({error:"Se necesita matricula"})
+        res.status(401).send({ error: "Se necesita matricula" })
     }
 })
 
@@ -38,25 +38,27 @@ app.post('/atencionNutricionalFormulario', async (req, res) => {
     const talla = req.body.talla
     const peso = req.body.peso
     const imc = req.body.imc
-    if(matricula && talla && peso && imc){
-        const obtenerIdPorMatricua =  `select id from usuarios where matricula = ${conn.escape(matricula)};`
+    if (matricula && talla && peso && imc) {
+        const obtenerIdPorMatricua = `select id from usuarios where matricula = ${conn.escape(matricula)};`
         conn.query(obtenerIdPorMatricua, (error, result, fileds) => {
-            if(error) 
-                res.status(500).send({error: error})
-            else {
-                const id = result[0].id
-                const insertarAtencionNutricionalFormulario = `INSERT INTO formularios_atencion_nutrimental (usuario_id, talla, peso, imc) VALUES (${conn.escape(id)}, ${conn.escape(talla)}, ${conn.escape(peso)}, ${conn.escape(imc)});`
-                conn.query(insertarAtencionNutricionalFormulario, (error, result, fileds) => {
-                    if(error) 
-                        res.status(500).send(error)
-                    else 
-                        res.status(200).send({status: "Operacion exitosa"})
-                })
-            }
+            if (error)
+                res.status(500).send(error)
+            else 
+                res.status(200).send(result)        
         })
     } else {
-        res.status(401).send({error:"Se necesita talla, peso e imc del usuario"})
+        res.status(401).send({ error: "Se necesita talla, peso e imc del usuario" })
     }
+})
+
+app.get('/clasificacionesEnfermedades', async (req, res) => {
+    const obtenerIdPorMatricua = `select * from calisficaciones_enfermedades;`
+    conn.query(obtenerIdPorMatricua, (error, result, fileds) => {
+        if (error)
+            res.status(500).send(error)
+        else
+            res.status(200).send(result)
+    })
 })
 
 const printMessage = () => { console.log('Running at http://localhost:3000/') }
